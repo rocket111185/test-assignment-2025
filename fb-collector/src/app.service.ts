69f11080925bbaaa0a6c5b4e58cc3b0758fdaa50
Dmytro Rekechynskyi <rocket111185@gmail.com>
2025-07-27 20:48:57 +0300

    Injected zod validator into fb-collector

--- fb-collector/src/app.service.ts
+++ fb-collector/src/app.service.ts
@@ -1,33 +1,54 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { NatsConnection, StringCodec, connect, ConsumerMessages } from 'nats';
-import { Event } from './events';
+import { NatsConnection, StringCodec, connect, AckPolicy, Consumer } from 'nats';
+import { FacebookEventSchema, FacebookEvent } from './validator';
 
 @Injectable()
 export class AppService implements OnModuleInit, OnModuleDestroy {
   private connection: NatsConnection;
+  private consumer: Consumer
 
-  async onModuleInit() {
+  async initializeNats() {
     this.connection = await connect({
       servers: process.env.NATS_URL
     });
 
-    const codec = StringCodec();
     const js = this.connection.jetstream();
+    const jsm = await this.connection.jetstreamManager();
+    const STREAM_NAME = 'events';
+    const CONSUMER_NAME = 'facebook-consumer';
 
-    const consumer = await js.consumers.get('events');
-    const messages = await consumer.consume();
+    await jsm.consumers.add(STREAM_NAME, {
+      durable_name: CONSUMER_NAME,
+      filter_subject: 'facebook',
+      ack_policy: AckPolicy.Explicit
+    });
 
-    // Consume messages using async iterator
+    this.consumer = await js.consumers.get(STREAM_NAME, CONSUMER_NAME);
+  }
+
+  async onModuleInit() {
+    await this.initializeNats();
+    const codec = StringCodec();
+    const messages = await this.consumer.consume();
+
     try {
       for await (const msg of messages) {
-        const decoded: Event = JSON.parse(codec.decode(msg.data));
-        console.log('[Consumer] Received:', decoded);
+        const decoded = JSON.parse(codec.decode(msg.data));
+        const validated = await FacebookEventSchema.safeParseAsync(decoded);
+
+        if (validated.success) {
+          const data: FacebookEvent = validated.data;
+          console.log('Received an object, sourco:', data.source);
+        } else {
+          console.error('This data piece was malfunctioned, sourco:', decoded.source);
+        }
+
         msg.ack();
       }
 
-      console.log('[Consumer] Message stream closed');
+      console.log('Message stream closed');
     } catch (error) {
-      console.error('[Consumer] Error:', error);
+      console.error('Error:', error);
     }
   }
 
--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventHandlerModule } from './event-handler/event-handler.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [EventHandlerModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
